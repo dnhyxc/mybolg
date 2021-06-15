cc2f27085aaa397d2762bf8ac83efc92db0732f9
dengninghai <2640559435@qq.com>
2021-06-16 01:55:19 +0800

    update home icon

--- themes/dnhyxc/source-src/js/changeTheme.js
+++ themes/dnhyxc/source-src/js/changeTheme.js
@@ -22,61 +22,79 @@ function init() {
     })
   }
 
+  function isLight() {
+    if (sessionStorage.getItem('container')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   xin.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('xin');
     e.stopPropagation();
     setThemes.xin();
   }
 
   dao.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('dao');
     e.stopPropagation();
     setThemes.dao();
   }
 
   shui.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('shui');
     e.stopPropagation();
     setThemes.shui();
   }
 
   qiong.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('qiong');
     e.stopPropagation();
     setThemes.qiong();
   }
 
   chu.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('chu');
     e.stopPropagation();
     setThemes.chu();
   }
 
   zuo.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('zuo');
     e.stopPropagation();
     setThemes.zuo();
   }
 
   kan.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('kan');
     e.stopPropagation();
     setThemes.kan();
   }
 
   yun.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('yun');
     e.stopPropagation();
     setThemes.yun();
   }
 
   qi.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('qi');
     e.stopPropagation();
     setThemes.qi();
   }
 
   shi.onclick = function (e) {
+    if (isLight()) return;
     changeSSItems('shi');
     e.stopPropagation();
     setThemes.shi();
