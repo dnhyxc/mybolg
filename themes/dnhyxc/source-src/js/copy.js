c07316181e431959e0f87c3f325be227b3257791
dengninghai <2640559435@qq.com>
2021-08-18 15:51:58 +0800

    add Replication capability

---
+++ themes/dnhyxc/source-src/js/copy.js
@@ -0,0 +1,62 @@
+function init() {
+  const article = document.querySelector(".article-entry");
+  const code = article.querySelectorAll("figure");
+
+  const copyToClipboard = (str, dom) => {
+    const el = document.createElement("textarea");
+    el.value = str;
+    el.setAttribute("readonly", "");
+    el.style.position = "absolute";
+    el.style.left = "-9999px";
+    document.body.appendChild(el);
+
+    const selected =
+      document.getSelection().rangeCount > 0
+        ? document.getSelection().getRangeAt(0)
+        : false;
+
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+
+    if (selected) {
+      document.getSelection().removeAllRanges();
+      document.getSelection().addRange(selected);
+      console.log("复制成功");
+      dom.classList.add("showCopyInfo");
+      if (dom.getAttribute("class").includes("showCopyInfo")) {
+        clearTimeout(timer);
+        const timer = setTimeout(() => {
+          dom.classList.remove("showCopyInfo");
+        }, 1500);
+      }
+    }
+  };
+
+  if (document.body.clientWidth >= 800) {
+    code.forEach((i) => {
+      const btn = document.createElement("div");
+      const info = document.createElement("info");
+      btn.className = "copyBtn";
+      info.className = "copyInfo";
+      btn.innerHTML = "复制代码";
+      info.innerHTML = "复制成功";
+      i.previousElementSibling.style.position = "relative";
+      i.previousElementSibling.appendChild(btn);
+      i.previousElementSibling.appendChild(info);
+      btn.onclick = function () {
+        let code = "";
+        i.querySelector(".code")
+          .querySelectorAll(".line")
+          .forEach((i) => {
+            code += i.innerText + "\n";
+          });
+        copyToClipboard(code, info);
+      };
+    });
+  }
+}
+
+module.exports = {
+  init: init,
+};
--- themes/dnhyxc/source-src/js/main.js
+++ themes/dnhyxc/source-src/js/main.js
@@ -1,5 +1,5 @@
 // 样式
-import '../css/main.scss'
+import "../css/main.scss";
 // 上报
 // import './report'
 // 图片查看器
@@ -9,86 +9,97 @@ import '../css/main.scss'
 // 边缘
 // import Aside from './aside'
 
-import ChangeLight from './changelight'
+import ChangeLight from "./changelight";
 
-import atticleNarrow from './atticle-narrow'
+import atticleNarrow from "./atticle-narrow";
 
-import action from './action'
+import action from "./action";
 
-import Setmargin from './setmargin'
+import Setmargin from "./setmargin";
 
-import articleToc from './articleToc'
+import articleToc from "./articleToc";
 
-import changeTheme from './changeTheme'
+import changeTheme from "./changeTheme";
 
-import { addLoadEvent } from './util'
+import { addLoadEvent } from "./util";
 
-import * as setThemes from './setThemes'
+import * as setThemes from "./setThemes";
 
-const cover = document.querySelector('.cover');
-const hideCoverBtn = document.querySelector('.hideCoverBtn');
-const changeInfo = document.querySelector('.changeInfo');
+import copy from "./copy";
+
+const cover = document.querySelector(".cover");
+const hideCoverBtn = document.querySelector(".hideCoverBtn");
+const changeInfo = document.querySelector(".changeInfo");
 
 addLoadEvent(function () {
-	// Share.init()
-	// Viewer.init();
-	// Aside.init();
-	Setmargin.init();
-	action.init();
-	ChangeLight.init();
-	atticleNarrow.init();
-	articleToc.init();
-	changeTheme.init();
-})
+  // Share.init()
+  // Viewer.init();
+  // Aside.init();
+  Setmargin.init();
+  action.init();
+  ChangeLight.init();
+  atticleNarrow.init();
+  articleToc.init();
+  changeTheme.init();
+  copy.init();
+});
 
 function stopTouchmove(e) {
-	e.preventDefault();
+  e.preventDefault();
 }
 
 document.onreadystatechange = function () {
-	if (document.readyState === "complete") {
-		cover && cover.classList.add('hideCover');
-		if (document.body.clientWidth <= 800) {
-			document.body.style.position = 'relative';
-			document.body.style.width = '100%';
-			document.body.removeEventListener('touchmove', stopTouchmove, { passive: false });
-		}
+  if (document.readyState === "complete") {
+    cover && cover.classList.add("hideCover");
+    if (document.body.clientWidth <= 800) {
+      document.body.style.position = "relative";
+      document.body.style.width = "100%";
+      document.body.removeEventListener("touchmove", stopTouchmove, {
+        passive: false,
+      });
+    }
 
-		if (!sessionStorage.getItem('container') && !sessionStorage.getItem('changeInfo')) {
-			let timer = null;
-			changeInfo.style.display = 'block';
-			sessionStorage.setItem('changeInfo', true);
-			if (timer) clearTimeout(timer);
-			timer = setTimeout(() => {
-				changeInfo.style.display = 'none';
-			}, 3000);
-		}
-	} else if (document.readyState === "interactive") {
-		hideCoverBtn && hideCoverBtn.addEventListener('click', function () {
-			cover && cover.classList.add('hideCover');
-		})
-		if (sessionStorage.getItem('xin')) {
-			setThemes.xin();
-		} else if (sessionStorage.getItem('dao')) {
-			setThemes.dao();
-		} else if (sessionStorage.getItem('shui')) {
-			setThemes.shui();
-		} else if (sessionStorage.getItem('qiong')) {
-			setThemes.qiong();
-		} else if (sessionStorage.getItem('chu')) {
-			setThemes.chu();
-		} else if (sessionStorage.getItem('zuo')) {
-			setThemes.zuo();
-		} else if (sessionStorage.getItem('kan')) {
-			setThemes.kan();
-		} else if (sessionStorage.getItem('yun')) {
-			setThemes.yun();
-		} else if (sessionStorage.getItem('qi')) {
-			setThemes.qi();
-		} else {
-			setThemes.shi();
-		}
-	} else {
-		document.body.addEventListener('touchmove', stopTouchmove, { passive: false });
-	}
-}
+    if (
+      !sessionStorage.getItem("container") &&
+      !sessionStorage.getItem("changeInfo")
+    ) {
+      let timer = null;
+      changeInfo.style.display = "block";
+      sessionStorage.setItem("changeInfo", true);
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        changeInfo.style.display = "none";
+      }, 3000);
+    }
+  } else if (document.readyState === "interactive") {
+    hideCoverBtn &&
+      hideCoverBtn.addEventListener("click", function () {
+        cover && cover.classList.add("hideCover");
+      });
+    if (sessionStorage.getItem("xin")) {
+      setThemes.xin();
+    } else if (sessionStorage.getItem("dao")) {
+      setThemes.dao();
+    } else if (sessionStorage.getItem("shui")) {
+      setThemes.shui();
+    } else if (sessionStorage.getItem("qiong")) {
+      setThemes.qiong();
+    } else if (sessionStorage.getItem("chu")) {
+      setThemes.chu();
+    } else if (sessionStorage.getItem("zuo")) {
+      setThemes.zuo();
+    } else if (sessionStorage.getItem("kan")) {
+      setThemes.kan();
+    } else if (sessionStorage.getItem("yun")) {
+      setThemes.yun();
+    } else if (sessionStorage.getItem("qi")) {
+      setThemes.qi();
+    } else {
+      setThemes.shi();
+    }
+  } else {
+    document.body.addEventListener("touchmove", stopTouchmove, {
+      passive: false,
+    });
+  }
+};
