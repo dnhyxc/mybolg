b267140c2a870db5bab3bd33d115b8abf4a5bb6d
dnhyxc <2640559435@qq.com>
2021-09-05 23:43:24 +0800

    fix some bug

--- themes/dnhyxc/source-src/js/action.js
+++ themes/dnhyxc/source-src/js/action.js
@@ -25,8 +25,8 @@ function init() {
   let leftCol_bar = document.querySelector(".leftCol_bar");
   let headerBar = document.querySelector(".headerBar");
   let header_bar = document.querySelector(".header_bar");
-  let footerBar = document.querySelector(".footerBar");
-  let footer_bar = document.querySelector(".footer_bar");
+  let footerBar = document.querySelectorAll(".footerBar");
+  let footer_bar = document.querySelectorAll(".footer_bar");
 
   let bodyScroll = document.body;
   let coverInfo = document.querySelectorAll(".coverInfo");
@@ -65,7 +65,7 @@ function init() {
     };
   });
 
-  scrollTop.forEach(i => {
+  scrollTop.forEach((i) => {
     i.onclick = function () {
       const clock = setInterval(function () {
         if (wrapper.scrollTop !== 0) {
@@ -75,13 +75,13 @@ function init() {
         }
       }, 10);
     };
-  })
+  });
 
   window.onresize = function () {
     if (bodyScroll.clientWidth <= 800) {
-      scrollTop.forEach(i => {
+      scrollTop.forEach((i) => {
         i.style.display = "none";
-      })
+      });
     }
   };
 
@@ -119,9 +119,9 @@ function init() {
     const countInfo = `${parseInt(
       (wrapper.scrollTop / (wrapper.scrollHeight - wrapper.offsetHeight)) * 100
     )}%`;
-    scrollCount.forEach(i => {
+    scrollCount.forEach((i) => {
       i.innerHTML = countInfo;
-    })
+    });
     topBar.style.width = countInfo;
     bottomBar.style.width = countInfo;
     rightBar.style.height = countInfo;
@@ -131,11 +131,13 @@ function init() {
     });
     leftCol_bar.style.height = countInfo;
     header_bar.style.width = countInfo;
-    footer_bar.style.width = countInfo;
+    footer_bar.forEach((i) => {
+      i.style.width = countInfo;
+    });
     if (bodyScroll.clientWidth <= 800) {
-      scrollTop.forEach(i => {
+      scrollTop.forEach((i) => {
         i.style.display = "none";
-      })
+      });
       scroll_top.style.display = "none";
       scrollbottom.style.display = "none";
       scroll_right.style.display = "none";
@@ -144,13 +146,16 @@ function init() {
         i.style.display = "none";
       });
       leftColBar.style.display = "none";
+      s;
       headerBar.style.display = "none";
-      footerBar.style.display = "none";
+      footerBar.forEach((i) => {
+        i.style.display = "none";
+      });
     } else {
       if (wrapper.scrollTop === 0) {
-        scrollTop.forEach(i => {
+        scrollTop.forEach((i) => {
           i.style.display = "none";
-        })
+        });
         scroll_top.style.display = "none";
         scrollbottom.style.display = "none";
         scroll_right.style.display = "none";
@@ -160,15 +165,17 @@ function init() {
         });
         leftColBar.style.display = "none";
         headerBar.style.display = "none";
-        footerBar.style.display = "none";
+        footerBar.forEach((i) => {
+          i.style.display = "none";
+        });
         clearTimeout(timer);
       } else if (
         Math.floor(wrapper.scrollHeight - wrapper.scrollTop) <=
         wrapper.clientHeight
       ) {
-        scrollTop.forEach(i => {
+        scrollTop.forEach((i) => {
           i.style.display = "block";
-        })
+        });
         scroll_top.style.display = "block";
         scrollbottom.style.display = "block";
         scroll_right.style.display = "block";
@@ -178,12 +185,14 @@ function init() {
         });
         leftColBar.style.display = "block";
         headerBar.style.display = "block";
-        footerBar.style.display = "block";
+        footerBar.forEach((i) => {
+          i.style.display = "block";
+        });
         clearTimeout(timer);
       } else {
-        scrollTop.forEach(i => {
+        scrollTop.forEach((i) => {
           i.style.display = "block";
-        })
+        });
         scroll_top.style.display = "block";
         scrollbottom.style.display = "block";
         scroll_right.style.display = "block";
@@ -193,12 +202,14 @@ function init() {
         });
         leftColBar.style.display = "block";
         headerBar.style.display = "block";
-        footerBar.style.display = "block";
+        footerBar.forEach((i) => {
+          i.style.display = "block";
+        });
         clearTimeout(timer);
         timer = setTimeout(() => {
-          scrollTop.forEach(i => {
+          scrollTop.forEach((i) => {
             i.style.display = "none";
-          })
+          });
           scroll_top.style.display = "none";
           scrollbottom.style.display = "none";
           scroll_right.style.display = "none";
@@ -208,12 +219,17 @@ function init() {
           });
           leftColBar.style.display = "none";
           headerBar.style.display = "none";
-          footerBar.style.display = "none";
+          footerBar.forEach((i) => {
+            i.style.display = "none";
+          });
         }, 2000);
       }
     }
 
-    if (Utils.isInformal || Utils.isArticle && !Utils.isArchives && !Utils.isCategories) {
+    if (
+      Utils.isInformal ||
+      (Utils.isArticle && !Utils.isArchives && !Utils.isCategories)
+    ) {
       if (wrapper.scrollTop <= 0) {
         articleToc.style.height = "calc(100vh - 225px)";
       } else {
@@ -251,7 +267,9 @@ function init() {
       }
 
       if (wrapper.scrollTop === 0) {
-        isSelect.length && isSelect[0].classList && isSelect[0].classList.remove("select-toc");
+        isSelect.length &&
+          isSelect[0].classList &&
+          isSelect[0].classList.remove("select-toc");
         mainLoading.innerHTML = mainLoadingText;
         mainLoading.title = mainLoadingText;
       }
@@ -303,7 +321,7 @@ function init() {
 
   document.addEventListener("mousemove", debounce(wrapperOnMouseMove, 100));
 
-  scrollTop.forEach(i => {
+  scrollTop.forEach((i) => {
     i.onmouseenter = function () {
       clearTimeout(timer);
       i.style.display = "block";
@@ -316,11 +334,13 @@ function init() {
       });
       leftColBar.style.display = "block";
       headerBar.style.display = "block";
-      footerBar.style.display = "block";
+      footerBar.forEach((i) => {
+        i.style.display = "block";
+      });
     };
-  })
+  });
 
-  scrollTop.forEach(i => {
+  scrollTop.forEach((i) => {
     i.onmouseleave = function () {
       if (
         Math.floor(wrapper.scrollHeight - wrapper.scrollTop) <=
@@ -337,7 +357,9 @@ function init() {
         });
         leftColBar.style.display = "block";
         headerBar.style.display = "block";
-        footerBar.style.display = "block";
+        footerBar.forEach((i) => {
+          i.style.display = "block";
+        });
       } else {
         timer = setTimeout(() => {
           i.style.display = "none";
@@ -350,11 +372,13 @@ function init() {
           });
           leftColBar.style.display = "none";
           headerBar.style.display = "none";
-          footerBar.style.display = "none";
+          footerBar.forEach((i) => {
+            i.style.display = "none";
+          });
         }, 2000);
       }
     };
-  })
+  });
 
   if (decodeURIComponent(path)) {
     if (path !== "/") {
@@ -365,14 +389,20 @@ function init() {
         ) + 1
       );
 
-      if (res === '/') {
-        const newPath = decodeURIComponent(path).slice(0, decodeURIComponent(path).length - 2)
+      if (res === "/") {
+        const newPath = decodeURIComponent(path).slice(
+          0,
+          decodeURIComponent(path).length - 2
+        );
         const res = decodeURIComponent(newPath).substr(
           decodeURIComponent(newPath).lastIndexOf(
             "/",
             decodeURIComponent(newPath).lastIndexOf("/") - 1
-          ) + 1)
-        mainLoading.innerHTML = `${res.split('/')[0][0].toUpperCase() + res.split('/')[0].slice(1)}-${res.split('/')[1]}`;
+          ) + 1
+        );
+        mainLoading.innerHTML = `${
+          res.split("/")[0][0].toUpperCase() + res.split("/")[0].slice(1)
+        }-${res.split("/")[1]}`;
       } else {
         const subPath = res.slice(0, res.length - 1);
         if (subPath === "informal") {
@@ -380,7 +410,11 @@ function init() {
           mainLoading.innerHTML = "Informal Essay";
           mainLoading.innerHTML = "Informal Essay";
           mainLoadingText = "Informal Essay";
-        } else if (Utils.isArticle && !Utils.isCategories && !Utils.isArchives) {
+        } else if (
+          Utils.isArticle &&
+          !Utils.isCategories &&
+          !Utils.isArchives
+        ) {
           changeSize.style.display = "block";
           mainLoading.innerHTML =
             "Article-" + subPath[0].toUpperCase() + subPath.slice(1);
