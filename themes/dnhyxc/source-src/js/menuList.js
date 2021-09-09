0503e1f937f3f8f2f64f2b875a177bad1f2e14d7
dnhyxc <2640559435@qq.com>
2021-09-10 00:14:35 +0800

    update scroll top

--- themes/dnhyxc/source-src/js/menuList.js
+++ themes/dnhyxc/source-src/js/menuList.js
@@ -1,64 +1,78 @@
-import * as Utils from './public-utils'
+import * as Utils from "./public-utils";
 
 function hideFooter() {
-  const endFooter = document.querySelector('.end-footer')
-  const afterFooter = document.querySelector('.after-footer')
-  const bodyWrap = document.querySelector('.body-wrap')
-  const articleToc = document.querySelector('.article-toc')
-  const browserCover = document.querySelector('.browserCover')
-  const perceptionLife  = document.querySelector('.perception-life')
+  const endFooter = document.querySelector(".end-footer");
+  const afterFooter = document.querySelector(".after-footer");
+  const bodyWrap = document.querySelector(".body-wrap");
+  const articleToc = document.querySelector(".article-toc");
+  const browserCover = document.querySelector(".browserCover");
+  const perceptionLife = document.querySelector(".perception-life");
+  const scrollWrap = document.querySelectorAll(".scrollWrap");
+  const changeFooter = document.querySelector(".changeFooter");
 
-  endFooter && endFooter.classList.add('toggleEndFooter')
-  afterFooter && afterFooter.classList.add('toggleAfterFooter')
-  bodyWrap && bodyWrap.classList.add('toggleWrap')
-  articleToc && articleToc.classList.add('toggleArticleToc')
-  browserCover && browserCover.classList.add('toggleBrowserCover')
-  perceptionLife && perceptionLife.classList.add('togglePerceptionLife')
+  endFooter && endFooter.classList.add("toggleEndFooter");
+  afterFooter && afterFooter.classList.add("toggleAfterFooter");
+  bodyWrap && bodyWrap.classList.add("toggleWrap");
+  articleToc && articleToc.classList.add("toggleArticleToc");
+  browserCover && browserCover.classList.add("toggleBrowserCover");
+  perceptionLife && perceptionLife.classList.add("togglePerceptionLife");
+  scrollWrap &&
+    scrollWrap.forEach((i) => {
+      i.classList.add("toggleScrollWrap");
+    });
+  changeFooter.innerHTML = "显底";
 }
 
 function showFooter() {
-  const endFooter = document.querySelector('.end-footer')
-  const afterFooter = document.querySelector('.after-footer')
-  const bodyWrap = document.querySelector('.body-wrap')
-  const articleToc = document.querySelector('.article-toc')
-  const browserCover = document.querySelector('.browserCover')
-  const perceptionLife  = document.querySelector('.perception-life')
+  const endFooter = document.querySelector(".end-footer");
+  const afterFooter = document.querySelector(".after-footer");
+  const bodyWrap = document.querySelector(".body-wrap");
+  const articleToc = document.querySelector(".article-toc");
+  const browserCover = document.querySelector(".browserCover");
+  const perceptionLife = document.querySelector(".perception-life");
+  const scrollWrap = document.querySelectorAll(".scrollWrap");
+  const changeFooter = document.querySelector(".changeFooter");
 
-  endFooter && endFooter.classList.remove('toggleEndFooter')
-  afterFooter && afterFooter.classList.remove('toggleAfterFooter')
-  bodyWrap && bodyWrap.classList.remove('toggleWrap')
-  articleToc && articleToc.classList.remove('toggleArticleToc')
-  browserCover && browserCover.classList.remove('toggleBrowserCover')
-  perceptionLife && perceptionLife.classList.remove('togglePerceptionLife')
+  endFooter && endFooter.classList.remove("toggleEndFooter");
+  afterFooter && afterFooter.classList.remove("toggleAfterFooter");
+  bodyWrap && bodyWrap.classList.remove("toggleWrap");
+  articleToc && articleToc.classList.remove("toggleArticleToc");
+  browserCover && browserCover.classList.remove("toggleBrowserCover");
+  perceptionLife && perceptionLife.classList.remove("togglePerceptionLife");
+  scrollWrap &&
+    scrollWrap.forEach((i) => {
+      i.classList.remove("toggleScrollWrap");
+    });
+  changeFooter.innerHTML = "隐底";
 }
 
 function init() {
-  const authorName = document.querySelector('.authorName')
-  const menuList = document.querySelector('.menuList')
-  const changeFooter = document.querySelector('.changeFooter')
+  const authorName = document.querySelector(".authorName");
+  const menuList = document.querySelector(".menuList");
+  const changeFooter = document.querySelector(".changeFooter");
 
   if (document.body.clientWidth > 800) {
-    authorName.addEventListener('click', () => {
-      menuList.classList.toggle('showMenu')
-    })
+    authorName.addEventListener("click", () => {
+      menuList.classList.toggle("showMenu");
+    });
 
-    changeFooter.addEventListener('click', () => {
-      if (Utils.getSSG('hideFooter')) {
-        Utils.removeSSG('hideFooter')
+    changeFooter.addEventListener("click", () => {
+      if (Utils.getSSG("hideFooter")) {
+        Utils.removeSSG("hideFooter");
       } else {
-        Utils.setSSG('hideFooter', true)
+        Utils.setSSG("hideFooter", true);
       }
-      if (Utils.getSSG('hideFooter')) {
-        hideFooter()
+      if (Utils.getSSG("hideFooter")) {
+        hideFooter();
       } else {
-        showFooter()
+        showFooter();
       }
-    })
+    });
   }
 }
 
 module.exports = {
   init: init,
   hideFooter,
-  showFooter
-}
+  showFooter,
+};
