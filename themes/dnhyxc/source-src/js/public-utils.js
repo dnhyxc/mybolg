8f52dcf4973172ec0a1d36239e3790f563d9417e
dnhyxc <2640559435@qq.com>
2021-10-22 22:44:10 +0800

    update test

--- themes/dnhyxc/source-src/js/public-utils.js
+++ themes/dnhyxc/source-src/js/public-utils.js
@@ -78,6 +78,40 @@ const getSystem = function () {
 
 const DPR = window.devicePixelRatio;
 
+let getTimeState = () => {
+  // 获取当前时间
+  let timeNow = new Date();
+  // 获取当前小时
+  let hours = timeNow.getHours();
+  // 设置默认文字
+  let text = ``;
+  // 判断当前时间段
+  if (hours >= 0 && hours <= 10) {
+    text = `早上好`;
+  } else if (hours > 10 && hours <= 14) {
+    text = `中午好`;
+  } else if (hours > 14 && hours <= 18) {
+    text = `下午好`;
+  } else if (hours > 18 && hours <= 24) {
+    text = `晚上好`;
+  }
+  // 返回当前时间段对应的状态
+  return text;
+};
+
+const isDarkOfLight = () => {
+  // 获取当前时间
+  let timeNow = new Date();
+  // 获取当前小时
+  let hours = timeNow.getHours();
+
+  if (hours >= 0 && hours <= 18) {
+    return "light";
+  } else {
+    return "dark";
+  }
+};
+
 module.exports = {
   isArticle,
   isInformal,
@@ -92,4 +126,6 @@ module.exports = {
   toBottom,
   getSystem,
   DPR,
+  isDarkOfLight,
+  getTimeState,
 };
