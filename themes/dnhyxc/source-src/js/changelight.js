d017afa52dcaae9b190145f0dfa43e85f652e5e5
dnhyxc <2640559435@qq.com>
2021-10-24 00:17:59 +0800

    change light with time

--- themes/dnhyxc/source-src/js/changelight.js
+++ themes/dnhyxc/source-src/js/changelight.js
@@ -1,6 +1,76 @@
 import closeShadow from "./closeShadow";
 import * as Utils from "./public-utils";
 
+function setLight() {
+  const container = document.querySelector("#container");
+  const toolsCol = document.querySelector(".tools-col");
+  const btnctn = document.querySelector(".btnctn");
+  const intrudeLess = document.querySelector("#intrude-less");
+  const dark = document.querySelector(".dark");
+  const toggleMusic = document.querySelector(".toggleMusic");
+  const mobileDark = document.querySelector(".mobileDark");
+  const toTopDark = document.querySelector(".toTopDark");
+
+  if (Utils.getSSG("container")) {
+    container.classList.add("container");
+  } else {
+    container.classList.remove("container");
+  }
+
+  if (Utils.getSSG("light")) {
+    dark.classList.add("light");
+    toggleMusic.classList.add("light");
+    dark.innerHTML = "炫酷";
+  } else {
+    dark.classList.remove("light");
+    toggleMusic.classList.remove("light");
+    dark.innerHTML = "白天";
+  }
+  if (Utils.getSSG("mobileLight")) {
+    mobileDark.classList.add("mobileLight");
+    toTopDark.classList.add("mobileLight");
+    mobileDark.innerHTML = "炫酷";
+  } else {
+    mobileDark.classList.remove("mobileLight");
+    toTopDark.classList.remove("mobileLight");
+    mobileDark.innerHTML = "白天";
+  }
+
+  if (Utils.getSSG("lightBtnctn")) {
+    btnctn.classList.add("lightBtnctn");
+  } else {
+    btnctn.classList.remove("lightBtnctn");
+  }
+  if (Utils.getSSG("lightToolsCol")) {
+    toolsCol.classList.add("lightToolsCol");
+  } else {
+    toolsCol.classList.remove("lightToolsCol");
+  }
+  if (Utils.getSSG("lightIntrudeLess")) {
+    intrudeLess.classList.add("lightIntrudeLess");
+  } else {
+    intrudeLess.classList.remove("lightIntrudeLess");
+  }
+}
+
+function initLight() {
+  if (Utils.isDarkOfLight !== "light") {
+    Utils.setSSG("container", true);
+    Utils.setSSG("lightToolsCol", true);
+    Utils.setSSG("lightBtnctn", true);
+    Utils.setSSG("lightIntrudeLess", true);
+    Utils.setSSG("light", true);
+    Utils.setSSG("mobileLight", true);
+  } else {
+    Utils.removeSSG("container", true);
+    Utils.removeSSG("lightToolsCol", true);
+    Utils.removeSSG("lightBtnctn", true);
+    Utils.removeSSG("lightIntrudeLess", true);
+    Utils.removeSSG("light", true);
+    Utils.removeSSG("mobileLight", true);
+  }
+}
+
 function init() {
   const container = document.querySelector("#container");
   const toolsCol = document.querySelector(".tools-col");
@@ -12,6 +82,10 @@ function init() {
   const toTopDark = document.querySelector(".toTopDark");
   const changeInfo = document.querySelector(".changeInfo");
   const lightChangeInfo = document.querySelector(".lightChangeInfo");
+
+  if (!Utils.getSSG("clearTimeControl")) {
+    initLight();
+  }
 
   function toggleLight() {
     if (
@@ -76,8 +150,9 @@ function init() {
       Utils.setSSG("mobileLight", true);
     }
   }
-  // PC端头像
+
   dark.onclick = function () {
+    Utils.setSSG("clearTimeControl", true);
     toggleLight();
     if (Utils.getSSG("container")) {
       changeInfo.style.display = "none";
@@ -89,55 +164,17 @@ function init() {
       }
     }
   };
+
   // 移动端头像
   mobileDark.onclick = function () {
+    Utils.setSSG("clearTimeControl", true);
     toggleLight();
   };
 
-  if (Utils.getSSG("container")) {
-    container.classList.add("container");
-  } else {
-    container.classList.remove("container");
-  }
-
-  if (Utils.getSSG("light")) {
-    dark.classList.add("light");
-    toggleMusic.classList.add("light");
-    dark.innerHTML = "炫酷";
-  } else {
-    dark.classList.remove("light");
-    toggleMusic.classList.remove("light");
-    dark.innerHTML = "白天";
-  }
-  if (Utils.getSSG("mobileLight")) {
-    mobileDark.classList.add("mobileLight");
-    toTopDark.classList.add("mobileLight");
-    mobileDark.innerHTML = "炫酷";
-  } else {
-    mobileDark.classList.remove("mobileLight");
-    toTopDark.classList.remove("mobileLight");
-    mobileDark.innerHTML = "白天";
-  }
-
-  if (Utils.getSSG("lightBtnctn")) {
-    btnctn.classList.add("lightBtnctn");
-  } else {
-    btnctn.classList.remove("lightBtnctn");
-  }
-  if (Utils.getSSG("lightToolsCol")) {
-    toolsCol.classList.add("lightToolsCol");
-  } else {
-    toolsCol.classList.remove("lightToolsCol");
-  }
-  if (Utils.getSSG("lightIntrudeLess")) {
-    intrudeLess.classList.add("lightIntrudeLess");
-  } else {
-    intrudeLess.classList.remove("lightIntrudeLess");
-  }
+  setLight();
 
   let timer;
   document.querySelector("#container").onscroll = function () {
-    // document.body.onscroll = function () {
     mobileDark.style.visibility = "visible";
     mobileDark.style.transition = "all 0.5s";
     toTopDark.style.visibility = "visible";
@@ -166,4 +203,5 @@ function init() {
 
 module.exports = {
   init: init,
+  initLight,
 };
