f3203f45e04fabcb44f2e98081e4952ec88ef9ea
dnhyxc <2640559435@qq.com>
2021-11-28 00:20:16 +0800

    update some article

--- themes/dnhyxc/source-src/js/main.js
+++ themes/dnhyxc/source-src/js/main.js
@@ -4,9 +4,6 @@ import "../css/main.scss";
 // import './report'
 // 图片查看器
 // import Viewer from './viewer'
-// 分享
-// import Share from './share'
-// 边缘
 import Aside from "./aside";
 
 import ChangeLight from "./changelight";
