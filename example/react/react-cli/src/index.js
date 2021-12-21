b1dda4eac2080c854caf1f5755e3ac59ed1b14b9
dninghai <dengninghai@shinemo.com>
2021-12-21 21:34:48 +0800

    update example

--- example/react/react-cli/src/index.js
+++ example/react/react-cli/src/index.js
@@ -5,13 +5,26 @@ import "./index.css";
 
 const RenderCreate = ({ type }) => {
   const selfProps = {
-    name: "SubChild",
+    name: "SubContent",
     content: type === "create" ? "我是Create组件" : "我是Content组件",
   };
   return React.createElement(Sub.Create, selfProps);
 };
 
-ReactDOM.render(
-  <RenderCreate type="content" />,
-  document.getElementById("root")
-);
+const App = ({ type }) => {
+  const selfProps = {
+    name: "SubChild",
+    content: type === "create" ? "我是Create组件" : "我是Content组件",
+  };
+
+  return (
+    <div>
+      <div>{Sub.Create(selfProps)}</div>
+      <div>
+        <RenderCreate type="content" />
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App type="create" />, document.getElementById("root"));
