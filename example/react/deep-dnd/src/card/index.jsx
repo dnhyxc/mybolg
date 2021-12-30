f29c13deb8a36a1c0ce4a7ea3ccd13642eeddef6
dninghai <dengninghai@shinemo.com>
2021-12-30 17:43:35 +0800

    update deepdnd

--- example/react/deep-dnd/src/card/index.jsx
+++ example/react/deep-dnd/src/card/index.jsx
@@ -2,11 +2,11 @@
  * @Description: 用于移动的卡片
  * @Author: dnh
  * @Date: 2021-12-28 18:13:59
- * @LastEditTime: 2021-12-29 21:20:53
+ * @LastEditTime: 2021-12-30 10:34:42
  * @LastEditors: dnh
  * @FilePath: \example\react\deep-dnd\src\card\index.jsx
  */
-import { useRef } from "react";
+import { useRef, useCallback } from "react";
 import { useDrag, useDrop } from "react-dnd";
 import { TYPE } from "../utils";
 import "./index.css";
@@ -21,26 +21,40 @@ const Card = ({ name, id, index, moveCard }) => {
 
   drag(ref);
 
-  const [, drop] = useDrop({
+  const [{ isOver, dropClassName }, drop] = useDrop({
     accept: TYPE,
-    hover(item, monitor) {
-      const dragIndex = item.index;
-      const hoverIndex = index;
-
-      if (dragIndex === hoverIndex) return;
-      const { top, bottom } = ref.current.getBoundingClientRect();
-      const halfOfHoverHeight = (bottom - top) / 2;
-      const { y } = monitor.getClientOffset();
-      const hoverClientY = y - top;
-
-      if (
-        (dragIndex < hoverIndex && hoverClientY > halfOfHoverHeight) ||
-        (dragIndex > hoverIndex && hoverClientY < halfOfHoverHeight)
-      ) {
-        moveCard(dragIndex, hoverIndex);
-        item.index = hoverIndex;
+    collect: (monitor) => {
+      const { index: dragIndex } = monitor.getItem() || {};
+      if (dragIndex === index) {
+        return {};
       }
+      return {
+        isOver: monitor.isOver(),
+        dropClassName:
+          dragIndex < index ? " drop-over-downward" : " drop-over-upward",
+      };
     },
+    drop: (item) => {
+      moveCard(item.index, index);
+    },
+    // hover(item, monitor) {
+    //   const dragIndex = item.index;
+    //   const hoverIndex = index;
+
+    //   if (dragIndex === hoverIndex) return;
+    //   const { top, bottom } = ref.current.getBoundingClientRect();
+    //   const halfOfHoverHeight = (bottom - top) / 2;
+    //   const { y } = monitor.getClientOffset();
+    //   const hoverClientY = y - top;
+
+    //   if (
+    //     (dragIndex < hoverIndex && hoverClientY > halfOfHoverHeight) ||
+    //     (dragIndex > hoverIndex && hoverClientY < halfOfHoverHeight)
+    //   ) {
+    //     moveCard(dragIndex, hoverIndex);
+    //     item.index = hoverIndex;
+    //   }
+    // },
   });
 
   dragPreview(drop(ref));
--- example/react/deep-dnd/src/cardList/index.jsx
+++ example/react/deep-dnd/src/cardList/index.jsx
@@ -2,11 +2,12 @@
  * @Description: 卡片列表，作为移入card的容器
  * @Author: dnh
  * @Date: 2021-12-28 18:14:57
- * @LastEditTime: 2021-12-29 21:22:18
+ * @LastEditTime: 2021-12-30 10:44:56
  * @LastEditors: dnh
  * @FilePath: \example\react\deep-dnd\src\cardList\index.jsx
  */
 import { useDrop } from "react-dnd";
+import update from "immutability-helper";
 import { TYPE } from "../utils";
 import Card from "../card";
 import "./index.css";
@@ -17,10 +18,19 @@ const CardList = ({ cardList, changeCards }) => {
   });
 
   const moveCard = (dragIndex, hoverIndex) => {
-    const dragCard = cardList[dragIndex];
-    cardList.splice(dragIndex, 1);
-    dragCard && cardList.splice(hoverIndex, 0, dragCard);
-    changeCards([...cardList]);
+    // const dragCard = cardList[dragIndex];
+    // cardList.splice(dragIndex, 1);
+    // dragCard && cardList.splice(hoverIndex, 0, dragCard);
+    // changeCards([...cardList]);
+    const dragRow = cardList[dragIndex];
+    changeCards(
+      update(cardList, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, dragRow],
+        ],
+      })
+    );
   };
 
   return (
