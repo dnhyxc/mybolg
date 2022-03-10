3844a17b6f430d0d5d3cd58a04a3931f2f997c39
dnhyxc <2640559435@qq.com>
2022-03-10 22:47:41 +0800

    feat: update article

--- example/react/react-fiber/src/index.js
+++ example/react/react-fiber/src/index.js
@@ -4,38 +4,43 @@
  * - js 是单线程的，而且 UI 线程与 JS 线程是互斥的，因此就会导致阻塞页面渲染。
  */
 
-import React from 'react';
+import React from "react";
 
 const element = (
-  <div id='0' className='wrap'>
-    <div id='1'>dom1</div>
-    <div id='2'>dom2</div>
+  <div id="0" className="wrap">
+    <div id="1">dom1</div>
+    <div id="2">dom2</div>
   </div>
-)
+);
 
 // console.log(element)
 // const virtualDOM = JSON.stringify(element, null, 2)
 // console.log(virtualDOM)
 
 const render = (element, rootNode) => {
-  console.log(element, rootNode, 'rootNode')
-  const dom = document.createElement(element.type)
-  Object.keys(element.props).filter(i => i !== 'children').forEach(j => {
-    dom[j] = element.props[j]
-  })
+  console.log(element, rootNode, "rootNode");
+  const dom = document.createElement(element.type);
+  // 给创建的dom添加除了children之外的属性
+  Object.keys(element.props)
+    .filter((i) => i !== "children")
+    .forEach((j) => {
+      dom[j] = element.props[j];
+    });
   if (Array.isArray(element.props.children)) {
-    element.props.children.forEach(i => {
-      console.log(dom, 'dom>>>i')
-      render(i, dom)
-    })
+    // 如果子节点是一个数组，则说明子节点是一个元素节点，需要采用递归创建该元素
+    element.props.children.forEach((i) => {
+      console.log(dom, "dom>>>i");
+      render(i, dom);
+    });
   } else {
-    dom.innerHTML = element.props.children
-    console.log(dom, 'innerHtml')
+    // 如果子节点不是一个数组类型，则说明它是文本节点，直接复制即可
+    dom.innerHTML = element.props.children;
+    console.log(dom, "innerHtml");
   }
-  rootNode.appendChild(dom)
-  console.log(dom, 'appendChild')
-}
+  rootNode.appendChild(dom);
+  console.log(dom, "appendChild");
+};
 
-render(element, document.querySelector('#root'))
+render(element, document.querySelector("#root"));
 
 // ReactDOM.render(element, document.querySelector('#root'))
