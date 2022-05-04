4850d87c34973736e9a6f2609b7301a158a9c639
dnhyxc <2640559435@qq.com>
2022-05-04 21:34:17 +0800

    update some article

--- themes/dnhyxc/source-src/js/previewImg.js
+++ themes/dnhyxc/source-src/js/previewImg.js
@@ -1,3 +1,5 @@
+import * as Utils from './public-utils'
+
 function init() {
   const articleEntry = document.querySelector(".article-entry");
   const previewImage = document.querySelector(".previewImage");
@@ -12,10 +14,13 @@ function init() {
 
   if (!imgs) return;
 
-  if (imgs.length > 1) {
+  const bodyWidth = document.body.clientWidth;
+
+  if (imgs.length > 1 && bodyWidth > 800) {
     left.style.display = "block";
     right.style.display = "block";
-  }
+  };
+
 
   function getImg() {
     return imgContainer.querySelector("img");
@@ -23,7 +28,15 @@ function init() {
 
   let currentIndex = 0;
 
+  const fragment = document.createDocumentFragment();
   imgs.forEach((i, index) => {
+    const dot = document.createElement('span')
+    if (index === currentIndex) {
+      dot.className = 'img_dot current_dot'
+    } else {
+      dot.className = 'img_dot'
+    }
+    fragment.appendChild(dot)
     i.onclick = () => {
       currentIndex = index;
       const img = getImg();
@@ -34,47 +47,104 @@ function init() {
       imgContainer.appendChild(clonedNode);
       previewImage.style.display = "block";
     };
+  });
 
-    if (right && left) {
-      right.onclick = function () {
-        info.style.display = "none";
+  const dot_wrap = document.createElement('div')
+  dot_wrap.className = 'dot_wrap'
+  dot_wrap.appendChild(fragment)
+  console.log(dot_wrap, 'dot_wrap')
+  imgContainer.appendChild(dot_wrap)
 
+  if (right && left && bodyWidth > 800) {
+    right.onclick = function () {
+      info.style.display = "none";
+      const img = getImg();
+      if (img) {
+        imgContainer.removeChild(img);
+      }
+      currentIndex += 1;
+      if (currentIndex === imgs.length) {
+        currentIndex = 0;
+      }
+      const res = imgs[currentIndex].cloneNode(true);
+      imgContainer.appendChild(res);
+      allDot.forEach((i, index) => {
+        i.className = 'img_dot'
+        if (index === currentIndex) {
+          i.className = 'img_dot current_dot'
+        }
+      })
+    };
+
+    left.onclick = function () {
+      info.style.display = "none";
+      const img = getImg();
+      if (img) {
+        imgContainer.removeChild(img);
+      }
+      if (currentIndex === 0) {
+        currentIndex = imgs.length;
+      }
+      if (currentIndex > 0) {
+        currentIndex -= 1;
+      }
+      const res = imgs[currentIndex].cloneNode(true);
+      imgContainer.appendChild(res);
+      allDot.forEach((i, index) => {
+        i.className = 'img_dot'
+        if (index === currentIndex) {
+          i.className = 'img_dot current_dot'
+        }
+      })
+    };
+  }
+
+  const allDot = dot_wrap.querySelectorAll('.img_dot')
+  console.log(allDot)
+
+  if (bodyWidth <= 800) {
+    if (!imgContainer) return
+    Utils.bindSwipeEvent(
+      imgContainer,
+      () => {
         const img = getImg();
         if (img) {
           imgContainer.removeChild(img);
         }
-
         currentIndex += 1;
-
         if (currentIndex === imgs.length) {
           currentIndex = 0;
         }
-
         const res = imgs[currentIndex].cloneNode(true);
         imgContainer.appendChild(res);
-      };
-
-      left.onclick = function () {
-        info.style.display = "none";
-
+        allDot.forEach((i, index) => {
+          i.className = 'img_dot'
+          if (index === currentIndex) {
+            i.className = 'img_dot current_dot'
+          }
+        })
+      },
+      () => {
         const img = getImg();
         if (img) {
           imgContainer.removeChild(img);
         }
-
         if (currentIndex === 0) {
           currentIndex = imgs.length;
         }
-
         if (currentIndex > 0) {
           currentIndex -= 1;
         }
-
         const res = imgs[currentIndex].cloneNode(true);
         imgContainer.appendChild(res);
-      };
-    }
-  });
+        allDot.forEach((i, index) => {
+          i.className = 'img_dot'
+          if (index === currentIndex) {
+            i.className = 'img_dot current_dot'
+          }
+        })
+      });
+  }
 
   previewImage.onclick = (e) => {
     if (e.target.id === "img_container") {
@@ -89,8 +159,15 @@ function init() {
   };
 
   fangda.onclick = () => {
+    if (bodyWidth <= 800) return
     const img = getImg();
-    const maxWidth = 1080 * 5; // 放大的极限值
+    let maxWidth // 放大的极限值
+    if (bodyWidth > 800) {
+      maxWidth = bodyWidth;
+    } else {
+      img.style.maxWidth = 'none'
+      maxWidth = bodyWidth * 2;
+    }
     if (img.width < maxWidth) {
       img.width = img.width * 1.2;
       info.style.display = "none";
@@ -101,6 +178,7 @@ function init() {
   };
 
   suoxiao.onclick = () => {
+    if (bodyWidth <= 800) return
     const img = getImg();
     const minWidth = 100;
     if (img.width > minWidth) {
